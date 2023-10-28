59255223dcd57e39bbeebbe73ae85efc6def232e
Kellercpv <xiaoup10086@gmail.com>
2023-10-28 21:43:18 +0800

    fix:测试commit签名

--- action/src/main/java/com/example/action/controller/Test.java
+++ action/src/main/java/com/example/action/controller/Test.java
@@ -1,5 +1,12 @@
-package com.example.action.controller;/**
+package com.example.action.controller;
+
+/**
  * @author hjq
  * @date 2023/10/28 21:34
- */public class Test {
+ */
+public class Test {
+    public void test(){
+
+    }
+
 }
